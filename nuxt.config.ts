c4b47892e2cb8dcd1978b0ee6b8e597ad9a9fc8d
Frederico Baumgratz <baumgratz.fred@gmail.com>
2025-10-08 10:13:08 -0300

    Adicionando title do projeto

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,7 +14,7 @@ export default defineNuxtConfig({
 
   app: {
     head: {
-      title: 'nox-nuxt-v4',
+      title: 'Workout Track',
       meta: [
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
         { name: 'theme-color', content: '#111827' }
